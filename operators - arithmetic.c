51cbcb4c7c0cb3653c0c301bf2d794f21b069f6b
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-08 09:23:19 +0530

    Add arithmetic operator program

---
+++ operators - arithmetic.c
@@ -0,0 +1,16 @@
+// OPERATOR IN C LANGUAGE //
+/* ARITHMATIC OPERATOR */
+#include <stdio.h>
+int main()
+{
+    int a ,b;
+    a = 3;
+    b = 2;
+    
+    printf("a-b = %d\n" ,a-b); 
+    printf("a+b = %d\n" ,a+b);    
+    printf("a*b = %d\n" ,a*b); 
+    printf("a/b = %d\n" ,a/b); 
+    
+    return 0;
+}
