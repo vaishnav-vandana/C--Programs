75ffcff9af194b437bc73f43ecf10bfb93969d3d
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-08 09:27:39 +0530

    Add relational operator program

---
+++ operators - relational.c
@@ -0,0 +1,17 @@
+/*RELATIONAL OPERATOR */
+#include <stdio.h>
+int main()
+{
+    int a , b;
+    a = 2;
+    b = 1;
+
+    printf("a==b = %d\n" , a==b);                                      //is equal to 
+    printf("a!=b = %d\n" , a!=b);                                      // is not equal to 
+    printf("a>=b = %d\n" , a>=b);                                      // greater than or equal to 
+    printf("a<=b = %d\n" , a<=b);                                      // less than or equal to 
+    printf("a>b = %d\n" , a>b);                                        // greater than 
+    printf("a<b = %d\n" , a<b);                                        // less than 
+
+    return 0 ; 
+}
