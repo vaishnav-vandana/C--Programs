14e6a7ff6c75f67074d20d10cc7a68208c04a46b
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-08 10:36:10 +0530

    Added logical operator program

---
+++ operators-logical.c
@@ -0,0 +1,15 @@
+/*LOGICAL OPERATOR */
+#include <stdio.h>
+int main ()
+{
+    int a,b;
+    a = 0;
+    b = 1;
+
+    printf("a&&b %d\n", a&&b);                                            // logical AND operator - both the operators are non zero (FALSE 0)
+    printf("a||b %d\n", a||b);                                            // logical OR operator  - both the operators are non zero (TRUE 1)
+    printf("!(a&&b) %d\n", !(a&&b));                                      // logical NOT operator - if condition is true than it print reverse of it.
+
+    return 0 ;
+
+}
