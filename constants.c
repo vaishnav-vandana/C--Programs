63642170723eba20066457562d3fd3bc3a546d98
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-10 10:41:11 +0530

    Update constants.c

--- constants.c
+++ constants.c
@@ -1,12 +1,19 @@
 /*USE OF CONSTANTS IN C */
 #include <stdio.h>
+
 int main()
 {
     int a = 8;
     const float b = 7.333;
-    //b = 7.22 ;  it gives an error . we can't do this since b is constant 
+    //b = 7.22 ;  it gives an error . we can't do this since b is constant or ERROR : CANNOT MODIFY CONSTANT
 
     printf("a = %d\n" , a);
-    printf("b = %f\n", b);    
+    printf("b = %f\n", b);   
+    
     return 0;
 }
+
+/* OUTPUT:
+a = 8
+b = 7.333000
+*/
