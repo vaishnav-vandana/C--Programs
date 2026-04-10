fbcbfc25a61ed68d16aad1304fa0985d8ca2d46e
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-10 10:52:14 +0530

    Update format-specifier.c

--- format-specifier.c
+++ format-specifier.c
@@ -1,12 +1,16 @@
-/*USE OF FORMAT SPECIFIER */
+/* USE OF FORMAT SPECIFIER IN C */
 #include <stdio.h>
+
 int main()
 {
-    int a=8;
-    float b=7.333;
-    printf("The value of a is %d and the value of b i %f\n",a,b);
+    int a = 8;
+    float b = 7.333;
+    
+    printf("The value of a is %d and the value of b is %f\n",a, b);
+    
     return 0;
 }
 
-
-
+/* OUTPUT:
+The value of a is 8 and the value of b is 7.333000
+*/
