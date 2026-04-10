29e71bc9ceb3576660aaaaae761cd6d2c055ca93
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-10 09:38:07 +0530

    Added format specifier program

---
+++ format-specifier.c
@@ -0,0 +1,12 @@
+/*USE OF FORMAT SPECIFIER */
+#include <stdio.h>
+int main()
+{
+    int a=8;
+    float b=7.333;
+    printf("The value of a is %d and the value of b i %f\n",a,b);
+    return 0;
+}
+
+
+
