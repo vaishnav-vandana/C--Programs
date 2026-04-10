111002b491036bba51d695fcd584c481dea7b573
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-10 10:48:16 +0530

    Update define-preprocessor.c

--- define-preprocessor.c
+++ define-preprocessor.c
@@ -1,11 +1,21 @@
-/*USE OF DEFINE PREPROCESSOR*/
+/* USE OF DEFINE PREPROCESSOR IN C */
 #include <stdio.h>
+
 # define PI 3.14
+
 int main()
 {
     int a = 8;
-    //PI = 7.33  it gives an error PI is in define preprocessor 
-    printf("a = %d\n",a);
-    printf("%f\n", PI);
-    return 0 ; 
+    
+    //PI = 7.33  it gives an error PI is in define preprocessor
+    
+    printf("a = %d\n", a);
+    printf("PI = %f\n", PI);
+    
+    return 0; 
 }
+
+/* OUTPUT:
+a = 8
+PI = 3.140000
+*/
