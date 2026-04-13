761c62978d37af875d808b222a45e07deb0caa9c
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-13 10:01:28 +0530

    Added do while loop program

--- do-while-loop.c
+++ do-while-loop.c
@@ -1,9 +1,11 @@
 //LOOPS IN C LANGUAGE 
+
 /* DO WHILE LOOP IN C  */
 
 # include <stdio.h>
 
 int main()
+
 {
     int num , index = 0 ;
     printf ("Enter a number\n");
@@ -18,3 +20,12 @@ int main()
     return 0 ;
     
 }
+/*
+OUTPUT:
+Enter a number: 5                         // Input = user decides (can be different every time) , Output = always depends on that input
+1  
+2  
+3  
+4  
+5  
+*/
