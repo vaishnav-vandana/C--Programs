8aee6d9ab214fcbbc2e719018d26bce76d133492
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-13 09:56:36 +0530

    Added define preprocessor program

--- define-preprocessor.c
+++ define-preprocessor.c
@@ -1,4 +1,5 @@
 /* USE OF DEFINE PREPROCESSOR IN C */
+
 #include <stdio.h>
 
 # define PI 3.14
@@ -7,7 +8,7 @@ int main()
 {
     int a = 8;
     
-    //PI = 7.33  it gives an error PI is in define preprocessor
+    // PI = 7.33  ❌ ERROR (because PI is a macro, not a variable)
     
     printf("a = %d\n", a);
     printf("PI = %f\n", PI);
@@ -15,7 +16,7 @@ int main()
     return 0; 
 }
 
-/* OUTPUT:
-a = 8
+/* OUTPUT : 
+a = 8 
 PI = 3.140000
 */
