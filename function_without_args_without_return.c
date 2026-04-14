76ecea728dc1061242bd710327b5fa46ebfb493f
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-14 11:05:18 +0530

    Added function without arguments and without return value

---
+++ function_without_args_without_return.c
@@ -0,0 +1,19 @@
+/*Example : without arguments and without return value*/
+
+#include <stdio.h>
+
+void greet ()
+{
+    printf("HELLO , KRISHN \n");
+}
+
+int main()
+{
+    greet();
+    return 0 ;
+}
+
+/*
+OUTPUT:
+HELLO , KRISHN
+*/
