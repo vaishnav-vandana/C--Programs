70303ff02f086fea6e41e40b6713fe66eaa02389
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-14 09:21:38 +0530

    Added function with arguments and return value

---
+++ function.c
@@ -0,0 +1,27 @@
+// FUNCTIONS IN C 
+
+/* Example : with arguments and with return value */ 
+
+#include <stdio.h>
+
+int sum (int a , int b )
+{
+    return a + b ;
+}
+
+int main ()
+{
+    int a , b , c ;
+    a = 77 ;
+    b = 28 ; 
+    c = sum (a,b);
+    printf("The sum is %d \n" , c );
+
+    return 0 ;
+
+}
+
+/*
+OUTPUT:
+The sum is 105
+*/
