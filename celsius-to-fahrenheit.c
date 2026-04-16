eb3812d01fc85b1081eb83d8b1f84652d066b5d4
vaishnav-vandana <vaishnavvandana725@gmail.com>
2026-04-16 10:07:31 +0530

    Added celsius to fahrenheit conversion program using do-while loop

---
+++ celsius-to-fahrenheit.c
@@ -0,0 +1,45 @@
+/* celsius to fahrenheit conversion using loop */
+
+#include <stdio.h>
+
+int main()
+{
+    double celsius , fahrenheit ;
+    char choice ;
+
+    do
+    {
+        printf ("Enter the value in celsius : \n ");
+        scanf ("%lf" , &celsius);
+
+        fahrenheit = ((celsius*1.8) + 32 );
+
+        printf ("Value in fahrenheit = %lf \n" , fahrenheit);
+
+        printf("Do you want to continue ? (yes-y / no-n): \n");
+        scanf(" %c",&choice);
+
+        if (choice == 'y' || choice == 'Y')
+        {
+            printf("YOU ARE IN PROGRAM \n");
+        }
+        else
+        {
+            printf("EXIT PROGRAM \n");
+        }
+
+
+    } while (choice == 'y' || choice == 'Y');
+
+    return 0;
+    
+}
+/* OUTPUT :
+Enter value in celsius:
+Input given by user
+
+Value in fahrenheit  = result
+
+y → continue
+n → exit
+*/
